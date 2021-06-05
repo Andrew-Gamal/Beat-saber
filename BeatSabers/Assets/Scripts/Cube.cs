5d80b84edd6f7d8350d262c15e4fc421ce7ff979
andrew <andrewgemy69@gmail.com>
2021-06-05 02:14:35 +0200

    making Explosion

---
+++ BeatSabers/Assets/Scripts/Cube.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Cube : MonoBehaviour
+{
+    private static Cube _cube;
+    public static Cube Instance => _cube;
+
+    float CubeSpeed=1f;
+
+    public float cubeSize = 0.2f;
+    public int cubesInRow = 5;
+
+    float cubesPivotDistance;
+    Vector3 cubesPivot;
+
+    public float explosionForce = 50f;
+    public float explosionRadius = 4f;
+    public float explosionUpward = 0.4f;
+
+    void Awake()
+    {
+        _cube = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //calculate pivot distance
+        cubesPivotDistance = cubeSize * cubesInRow / 2;
+        //use this value to create pivot vector)
+        cubesPivot = new Vector3(cubesPivotDistance, cubesPivotDistance, cubesPivotDistance);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position += Time.deltaTime * (-transform.right) * (CubeSpeed * Menu.Instance.ValueChangeCheck());
+    }
+
+    public void explode()
+    {
+        //make object disappear
+        gameObject.SetActive(false);
+
+        //loop 3 times to create 5x5x5 pieces in x,y,z coordinates
+        createPiece(1, 1);
+        createPiece(1, 1);
+       
+
+        //get explosion position
+        Vector3 explosionPos = transform.position;
+        //get colliders in that position and radius
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, explosionRadius);
+        //add explosion force to all colliders in that overlap sphere
+        foreach (Collider hit in colliders)
+        {
+            //get rigidbody from collider object
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                //add explosion force to this body with given parameters
+                rb.AddExplosionForce(explosionForce, transform.position, explosionRadius, explosionUpward);
+            }
+        }
+
+    }
+
+    void createPiece(int x, int y)
+    {
+
+        //create piece
+        GameObject piece;
+        piece = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        //set piece position and scale
+        piece.transform.position = transform.position + new Vector3(cubeSize * x, cubeSize * y) - cubesPivot;
+        piece.transform.localScale = new Vector3(cubeSize, cubeSize, cubeSize);
+
+        //add rigidbody and set mass
+        piece.AddComponent<Rigidbody>();
+        piece.GetComponent<Rigidbody>().mass = cubeSize;
+    }
+}
